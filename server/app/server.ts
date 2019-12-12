203c177b0757d5b7dfbbc804ef8de0e361215fec
RafaelT <rafaeltanzerschool@gmail.com>
2019-12-12 13:39:06 +0100

    Inital Commit

---
+++ server/app/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+//import * as socketio from "socket.io";
+//import * as path from "path";
+
+const app = express();
+
+app.listen(3000, () => {
+ console.log('asskldjaldas');
+})
