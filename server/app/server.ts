e734f8746bcd13abdabc5ec84a1d50d825209722
Lukas Wirth <luke.wirth31@gmail.com>
2020-01-02 15:11:23 +0100

    Implemented basic parts of server

--- server/app/server.ts
+++ server/app/server.ts
@@ -1,9 +1,45 @@
 import express from "express";
+import http from "http";
 //import * as socketio from "socket.io";
 //import * as path from "path";
+import socketio from "socket.io";
 
+const port = 3000;
 const app = express();
+const server = new http.Server(app);
+const socketServer = socketio.listen(server, {
+    path: '/whatsapp',
+    // serveClient: false,
+    // pingInterval: 10000,
+    // pingTimeout: 5000,
+    // cookie: false
+});
 
-app.listen(3000, () => {
- console.log('asskldjaldas');
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
+socketServer.on('connection', ws => {
+    console.log('a user connected');
+
+    ws.on('login', data => {
+        console.log('login: ' + data);
+    });
+
+    ws.on('message', message => {
+        console.log('message: ' + message);
+        socketServer.emit('message', message);
+    });
+
+    ws.on('logout', data => {
+        console.log('logout: ' + data);
+    });
+
+    ws.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+});
+
+server.listen(port, () => {
+    console.log('listen on port: ' + port);
 });
--- server/build/server.js
+++ server/build/server.js
@@ -2,11 +2,37 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-//import * as socketio from "socket.io";
+var http_1 = __importDefault(require("http"));
+var socketio = __importStar(require("socket.io"));
 //import * as path from "path";
+var port = 3000;
 var app = express_1.default();
-app.listen(3000, function () {
-    console.log('asskldjaldas');
+var server = new http_1.default.Server(app);
+var socketServer = socketio.listen(server, {
+    path: '/whatsapp',
+});
+server.listen(port, function () {
+    console.log('listen on port: ' + port);
+});
+app.get('/', function (req, res) {
+    res.send('hello');
+});
+socketServer.on('connection', function (ws) {
+    console.log('a user connected');
+    ws.on('message', function (message) {
+        console.log('message: ' + message);
+        socketServer.emit('message', message);
+    });
+    ws.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 });
