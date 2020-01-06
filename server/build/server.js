bcfed3bebdbe9d625f5bd6a196c66de128196604
Lukas Wirth <luke.wirth31@gmail.com>
2020-01-06 16:02:13 +0100

    New build

--- server/build/server.js
+++ server/build/server.js
@@ -2,37 +2,37 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var http_1 = __importDefault(require("http"));
-var socketio = __importStar(require("socket.io"));
+//import * as socketio from "socket.io";
 //import * as path from "path";
+var socket_io_1 = __importDefault(require("socket.io"));
 var port = 3000;
 var app = express_1.default();
 var server = new http_1.default.Server(app);
-var socketServer = socketio.listen(server, {
+var socketServer = socket_io_1.default.listen(server, {
     path: '/whatsapp',
-});
-server.listen(port, function () {
-    console.log('listen on port: ' + port);
 });
 app.get('/', function (req, res) {
     res.send('hello');
 });
 socketServer.on('connection', function (ws) {
     console.log('a user connected');
+    ws.on('login', function (data) {
+        console.log('login: ' + data);
+    });
     ws.on('message', function (message) {
         console.log('message: ' + message);
         socketServer.emit('message', message);
     });
+    ws.on('logout', function (data) {
+        console.log('logout: ' + data);
+    });
     ws.on('disconnect', function () {
         console.log('user disconnected');
     });
+});
+server.listen(port, function () {
+    console.log('listen on port: ' + port);
 });
